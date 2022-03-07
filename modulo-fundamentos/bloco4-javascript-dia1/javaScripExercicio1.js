18cbd49b37aa3ca3ab3ec8ebef4f1b8d88126ce9
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-07 16:51:00 -0300

    exercicio 4

--- modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
+++ modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
@@ -57,4 +57,4 @@ else {
 
 
 
-//EXERCICIO 5
+//EXERCICIO 5 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
