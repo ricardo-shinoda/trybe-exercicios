f2803d869eb7b9c0d9913cf1b3571a6f8574c6c1
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-07 22:12:15 -0300

    exercise 10

--- modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
+++ modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
@@ -158,17 +158,56 @@
 // Exercicio 8 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 // Bonus: use somente um if .
 
-const num1 = 1;
-const num2 = 1;
-const num3 = 6;
+// const num1 = 1;
+// const num2 = 1;
+// const num3 = 6;
 
-if (num1 % 2 === 0 || num2 % 2 === 0 || num3 %2 === 0 ) {
-    console.log(true);
-}
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 %2 === 0 ) {
+//     console.log(true);
+// }
 
-else {
-    console.log(false);
-}
+// else {
+//     console.log(false);
+// }
+
+
+// Exercicio 9 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+// const num1 = 2;
+// const num2 = 0;
+// const num3 = 12;
+
+// if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 %2 !== 0 ) {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// }
+// 
+// Exercicio 10 - Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+
+// const custoProduto = 10;
+// const valorVenda = 50;
+// const lucro = valorVenda - ((custoProduto * 0.20) + custoProduto);
+
+// if (custoProduto < 0 && valorVenda < 0) {
+//     console.log("invalid imput");
+// }
+
+// else {
+//     console.log("Lucro total: ", lucro * 1000);
+//     console.log("Lucro por item: ", lucro);
+// }
+
+
 
 
 
