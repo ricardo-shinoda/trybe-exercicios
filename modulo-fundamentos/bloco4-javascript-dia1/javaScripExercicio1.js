17b76f468284a1934470a2c7b436568f9172e4c8
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-07 21:33:46 -0300

    exercise 6

--- modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
+++ modulo-fundamentos/bloco4-javascript-dia1/javaScripExercicio1.js
@@ -59,18 +59,59 @@
 
 //EXERCICIO 5 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-const angulo1 = 70;
-const angulo2 = 80;
-const angulo3 = 30;
+// const angulo1 = 70;
+// const angulo2 = 80;
+// const angulo3 = 30;
 
-if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
-    console.log("Angulo inválido")
+// if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+//     console.log("Angulo inválido")
+// }
+
+// else if (angulo1 + angulo2 + angulo3 == 180) {
+//     console.log("true");
+// }
+
+// else if (angulo1 + angulo2 + angulo3 !== 180) {
+//     console.log("false");
+// }
+
+
+// EXERCICIO 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+
+const nome = "king";
+
+switch (nome.toLocaleLowerCase()) {
+    case "bishop":
+        console.log("diogonal");
+        break;
+
+    case "knight":
+        console.log("L shape");
+        break;
+
+    case "pawn":
+        console.log("vertically");
+        break;
+    
+    case "rook":
+        console.log("horizontally/vertically");
+        break;
+
+    case "queen":
+        console.log("diagonally, horizontally or vertically");
+        break;
+
+    case "king":
+        console.log("one square, any direction");
+        break;
+
+
+    default:
+        console.log("Invalid name")
 }
 
-else if (angulo1 + angulo2 + angulo3 == 180) {
-    console.log("true");
-}
-
-else if (angulo1 + angulo2 + angulo3 !== 180) {
-    console.log("false");
-}
