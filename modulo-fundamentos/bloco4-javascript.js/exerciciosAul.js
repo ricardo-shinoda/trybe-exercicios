479546a07ab9155a0d66ea1be3747e21e5d2b4b8
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-08 17:26:08 -0300

    exercicios aula 4.2

--- modulo-fundamentos/bloco4-javascript.js/exerciciosAul.js
+++ modulo-fundamentos/bloco4-javascript.js/exerciciosAul.js
@@ -1,193 +1,193 @@
-// PRIMEIRO EXERCICIO
+//PRIMEIRO EXERCICIO
 
-// const myName = "Ricardo";
-// const birthCity = "Bauru";
-// // birthCity = "São Paulo";
-// let birthYear = 1980;
-// birthYear = 2010;
+const myName = "Ricardo";
+const birthCity = "Bauru";
+// birthCity = "São Paulo";
+let birthYear = 1980;
+birthYear = 2010;
 
-// console.log(myName);
-// console.log(birthCity);
-// console.log(birthYear);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 
-// SEGUNDO EXERCICIO
+//SEGUNDO EXERCICIO
 
-// let patientId = 50;
-// let isEnrolled = true;
-// const patientInfo = {
-//   firstName: 'Ana',
-//   lastName: 'Santos',
-// };
-// const patientEmail = 'ana@email.com';
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
 
 
-// typeof patientId
+typeof patientId
 
-// TERCEIRO EXERCICIO
+//TERCEIRO EXERCICIO
 
-// const base = 5;
-// let height = 8;
-// const area = base * height;
-// const perimeter = area * 2;
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = area * 2;
 
-// console.log(base);
-// console.log(height);
-// console.log(area);
-// console.log(perimeter);
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter);
 
-// if/else
+//if/else
 
-//  const nota = 70;
+ const nota = 70;
 
-//  if (nota >= 80) {
-//      console.log("Parabéns, você foi aprovado(a)");
+ if (nota >= 80) {
+     console.log("Parabéns, você foi aprovado(a)");
+ }
+
+ else if (nota < 80 && nota >= 60) {
+     console.log("Você está na nossa lidata de espera");
+ }
+
+//  else if (nota < 60) {
+//      console.log("Você foi reprovado(a)");
 //  }
 
-//  else if (nota < 80 && nota >= 60) {
-//      console.log("Você está na nossa lidata de espera");
-//  }
-
-// //  else if (nota < 60) {
-// //      console.log("Você foi reprovado(a)");
-// //  }
-
-// else {
-//     console.log("Você está reprovado(a)");
-// }
+else {
+    console.log("Você está reprovado(a)");
+}
 
 //OPERADORES LÓGICOS AND, OR, NOT
 
-// const comida = 'pão na chapa';
-// const bebida = 'cafézinho';
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
 
-// if (bebida === 'cafézinho' && comida === 'pão na chapa') {
-//   console.log('Muito obrigado pela refeição :)');
-// } else {
-//   console.log('Acho que houve um engano com meu pedido');
-// }
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
 
-// outro no caso de and as duas constantes devem ser verdadeiras
+//outro no caso de and as duas constantes devem ser verdadeiras
 
-// const conditionOne = true;
-// const conditionTwo = false;
+const conditionOne = true;
+const conditionTwo = false;
 
-// console.log(conditionOne && conditionTwo);
+console.log(conditionOne && conditionTwo);
 
-// outro
+outro
 
-// const cenouras = true;
-// const leite = true;
-// const arroz = true;
-// const feijao = true;
+const cenouras = true;
+const leite = true;
+const arroz = true;
+const feijao = true;
 
-// const listaDeCompras = cenouras && leite && arroz && feijao;
+const listaDeCompras = cenouras && leite && arroz && feijao;
 
-// console.log(true && true); // true
-// console.log(true && false); // false
-// console.log(false && true); // false
-// console.log(false && false); // false
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
 
-// Outro exercicio
+//Outro exercicio
 
-// const currentHour = 11;
-// let message = null;
+const currentHour = 11;
+let message = null;
 
-// if (currentHour >= 22) {
-//     message = "Não deveríamos comer nada, é hora de dormir";
-// }
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+}
 
-// else if (currentHour >= 18 && currentHour < 22) {
-//     message = "Rango da noite, vamos jantar :D";
-// }
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+}
 
-// else if (currentHour >= 14 && currentHour < 18) {
-//     message = "Vamos fazer um bolo pro café da tarde?";
-// }
+else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+}
 
-// else if (currentHour > 11 && currentHour < 14) {
-//     message = "Hora do almoço!!!";
-// }
+else if (currentHour > 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+}
 
-// else if (currentHour >= 4 && currentHour < 11) {
-//     message = "Hmmm, cheiro de café recém passado";
-// }
+else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém passado";
+}
 
-// console.log(message);
+console.log(message);
 
-// OPERADOR LÓGICO OR
+//OPERADOR LÓGICO OR
 
-// const bebidaPrincipal =  'cafézinho';
-// const bebidaAlternativa = 'suco de laranja';
+const bebidaPrincipal =  'cafézinho';
+const bebidaAlternativa = 'suco de laranja';
 
-// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
-//   console.log("Obrigado por me atender :D")
-// } else {
-//   console.log("Ei, eu não pedi isso!");
-// }
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+}
 
-// Tabela de resultados OR 
+//Tabela de resultados OR 
 
-// console.log(true || true); // true
-// console.log(true || false); // true
-// console.log(false || true); // true
-// console.log(false || false); // false
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
 
-// let weekDay = "domingo";
+let weekDay = "domingo";
 
-// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
-// }
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
 
-// else if (weekDay === "sábado" || weekDay === "domingo") {
-//     console.log("FINALMENTE, descanso merecido UwU")
-// }
+else if (weekDay === "sábado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU")
+}
 
-// OPERADOR LÓGICO NOT
+OPERADOR LÓGICO NOT
 
-// console.log((2 + 2) === 4); 
+console.log((2 + 2) === 4); 
 
-// console.log(!(2 + 2) === 4);
+console.log(!(2 + 2) === 4);
 
-// console.log(!42);
+console.log(!42);
 
-// console.log(!null); // true
+console.log(!null); // true
 
-// console.log(!undefined); // true
+console.log(!undefined); // true
 
 
 
-//SWIT CASE
+SWIT CASE
 
-// let estado = "aprovado";
+let estado = "aprovado";
 
-// switch (estado) {
-//     case "aprovado":
-//         console.log("aprovada");
-//         break;
+switch (estado) {
+    case "aprovado":
+        console.log("aprovada");
+        break;
 
-//     case "lista":
-//         console.log("lista");
-//         break;
+    case "lista":
+        console.log("lista");
+        break;
 
-//     case "reprovada":
-//         console.log("reprovada");
-//         break;
+    case "reprovada":
+        console.log("reprovada");
+        break;
 
-//     default:
-//         console.log("Não se aplica")
+    default:
+        console.log("Não se aplica")
 
         
-// }
+}
 
 
-// EXERCICIO ARRAYS AND FOR
+EXERCICIO ARRAYS AND FOR
 
-// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
-// console.log(tasksList.length);
+console.log(tasksList.length);
 
 
-// //SEARCHING FOR CERTAIN ITEM ON ARRAY:
+//SEARCHING FOR CERTAIN ITEM ON ARRAY:
 
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
---
+++ modulo-fundamentos/bloco4-javascript.js/javaScriptExercicio2.js
@@ -0,0 +1,116 @@
+//exercicio1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}
+
+//exercicio2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+    result += numbers[index];
+}
+
+console.log(result);
+
+//exercicio3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index];
+}
+
+let average = sum / numbers.length;
+
+console.log(average);
+
+
+//exercicio4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+    result += numbers[index];
+}
+
+result = result / numbers.length;
+
+if (result > 20) {
+    console.log("valor maior que 20");
+}
+
+else {
+    console.log("valor menor ou igual a 20");
+}
+
+//exercicio5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let largest = numbers[0];
+//for (inicialização, verificação, atualização)
+for (let index = 0; index < numbers.length; index += 1) {
+    if (largest < numbers[index]) {
+        largest = numbers[index];
+    }
+}
+
+console.log(largest);
+
+// exercicio 5 - em menos linhas
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let largest = Math.max.apply(0, numbers);
+console.log(largest);
+
+
+
+//exercicio6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        result += 1;
+    }
+}
+
+if (result === 0) {
+    console.log("nenhum valor ímpar encontrado");
+}
+else {
+    console.log(result);
+}
+
+
+// exercicio7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let minor = numbers[0];
+
+for (let index = 0; index < numbers.length; index++) {
+    if (minor > numbers[index]) {
+        minor = numbers[index];
+    }
+}
+
+console.log(minor);
+
+//exercicio8 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [];
+
+for(let index = 1; index <= 25; index++) {
+    numbers.push(index);
+}
+
+console.log(numbers);
+
