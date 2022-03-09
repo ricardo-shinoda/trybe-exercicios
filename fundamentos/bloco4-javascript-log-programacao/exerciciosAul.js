d4e7fcd98e73c06cb5d6306dc7856d646c7aaa02
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-09 09:31:19 -0300

    exercicio aula

--- fundamentos/bloco4-javascript-log-programacao/exerciciosAul.js
+++ fundamentos/bloco4-javascript-log-programacao/exerciciosAul.js
@@ -324,3 +324,26 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (numero of names) {
   console.log(numero);
 }
+
+
+//exercicio - Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (let index = 0; index < fruits.length; index++) {
+  soma += fruits[index];
+
+}
+
+if (soma > 15) {
+  console.log(soma);
+}
+
+else {
+  console.log("valor menor que 16");
+}
+
+
+
+
