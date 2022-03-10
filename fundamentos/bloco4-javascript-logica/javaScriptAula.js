3bd80497505484011e9f56a80ee4be0508cae354
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-10 15:06:50 -0300

    exercicios do dia

--- fundamentos/bloco4-javascript-logica/javaScriptAula.js
+++ fundamentos/bloco4-javascript-logica/javaScriptAula.js
@@ -468,3 +468,238 @@ else if (ana == ronald && ana < manu){
 else if (ronald == manu && ronald < ana) {
   console.log("Ronald e Manu são os mais novos");
 }
+
+// AULA 4.4 OBJETOS
+
+let singer = {
+  name: "Milton", 
+  lastName: "Nascimento",
+  nickname: "Bituca",
+  age: 77,
+  bestAlbuns: ["Travessia", "Clube da esquina", "Minas"]
+};
+
+console.log("O cantor " + singer.name + " " + singer.lastName + " possui " + singer.age + " anos");
+
+console.log("O cantor " + singer["name"] + " " + singer["lastName"] + " possui " + singer["age"] + " anos");
+
+
+//outro exemplo
+
+let diasDaSemana = {
+  : 'domingo',
+  : 'segunda',
+  : 'terça',
+  : 'quarta',
+  : 'quinta',
+  : 'sexta',
+  : 'sábado',
+  };
+  
+  diasDaSemana.1; // SyntaxError: Unexpected number
+  console.log(diasDaSemana['1']); // domingo
+
+  //outro
+  
+  let conta = {
+    agencia: '0000',
+    banco: {
+      cod: '012',
+      id: 4,
+      nome: 'TrybeBank',
+    },
+  };
+  
+  let infoDoBanco = 'banco';
+  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+  console.log(conta[infoDoBanco]['nome']); // TrybeBank
+  
+  console.log(conta.agencia); // 0000
+  console.log(conta['agencia']); // 0000
+  
+  console.log(conta.banco.cod); // 012
+  console.log(conta['banco']['id']); // 4
+
+
+  // outru
+
+  let usuario = {
+    id: 99,
+    email: 'jakeperalta@gmail.com',
+    infoPessoal: {
+      nome: 'Jake',
+      sobrenome: 'Peralta',
+      endereco: {
+        rua: 'Smith Street',
+        bairro: 'Brooklyn',
+        cidade: 'Nova Iorque',
+        estado: 'Nova Iorque',
+      },
+    },
+  };
+  
+  console.log(usuario['id']); // 99
+  console.log(usuario.id); //99
+  console.log(usuario.email); // jakeperalta@gmail.com
+  
+  console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+  console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+  console.log(usuario.infoPessoal.endereco.cidade);
+
+  // Acessando as propriedades de um array:
+
+  let moradores = [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ];
+  
+  let primeiroMorador = moradores[0];
+  console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+  console.log(primeiroMorador['andar']); // 10
+  
+  let ultimoMorador = moradores[moradores.length - 1];
+  console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+  console.log(ultimoMorador.nome); // Zoey
+
+
+  //exericio1 - 
+
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
+  // let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018],
+
+}
+
+
+//exercicio2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
+
+//exercicio3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+//exercicio4 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+let count = 0;
+for (index = 0; index < bestInTheWorld.length; index++) {
+    count += 1;
+}
+
+console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + count + " vezes");
+
+//exercicio5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+
+console.log("A jogadora possui: " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata");
+
+
+// FOR IN
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+// RESULTS: saab, volvo, bmw
+
+//outro
+
+let car = {
+  type: 'Fiat',
+  model: '500',
+  color: 'white',
+};
+
+for (let index in car) {
+  console.log(index, car[index]);
+}
+
+//resultado; 
+// type Fiat
+// model 500
+// color white
+
+//* for in
+let food = ['hamburguer', 'bife', 'acarajé'];
+for (let position in food) {
+  console.log(position);
+};
+//resultado: 0, 1, 2;
+
+//! FOR OF
+
+let food = ['hamburguer', 'bife', 'acarajé'];
+for (let value of food) {
+  console.log(value);
+};
+//resultado: hamburguer, bife, acarajé;
+
+
+// exercicio - Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let index in names) {
+  console.log("Olá " + names[index]);
+
+}
+
+//exercicio2 - Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto
+
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index in car) {
+  console.log(index, car[index]);
+}
+
+//! Funções:
+
+
+// Sem função
+let nome = 'João';
+
+console.log('Bom dia, ' + nome);
+
+
+// Com função
+function bomDiaTryber(nome) {
+  console.log('Bom dia, ' + nome);
+}
+
+bomDiaTryber('João'); // Bom dia, João
+bomDiaTryber('Julia'); // Bom dia, Julia
+bomDiaTryber('Marcelo'); // Bom dia, Marcelo
