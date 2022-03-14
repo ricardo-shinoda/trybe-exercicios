b498ac235eabe0f639dc306c872c6987ff2e72a7
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-03-14 15:41:05 -0300

    arrumando arquivos

--- fundamentos/bloco4-javascript-logica/javaScriptAula.js
+++ fundamentos/bloco4-javascript-logica/javaScriptAula.js
@@ -788,3 +788,22 @@ function biggerNum (num1, num2) {
 console.log(biggerNum(3, 4));
 console.log(biggerNum(35, 4));
 console.log(biggerNum(4, 4));
+
+//! DOM e SELETORES
+//! AULA 1:
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <header>
+      <h2 id="page-title">Título</h2>
+      <p id="paragraph">Dê uma cor para este parágrafo!</p>
+      <h4 id="subtitle">Subtítulo</h4>
+      <p id="second-paragraph">Segundo parágrafo!</p>
+    </header>
+    <script>
+      const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+    </script>
+  </body>
+</html>
