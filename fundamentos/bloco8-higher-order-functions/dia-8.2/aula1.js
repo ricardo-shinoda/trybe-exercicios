8ee9f71c764e503b4083a4dd924f2e6caef33892
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-04-10 22:18:20 -0300

    resolvendo alguns exercicios do dia 8.2

---
+++ fundamentos/bloco8-higher-order-functions/dia-8.2/aula1.js
@@ -0,0 +1,151 @@
+
+// Exercício 1 - Saber de acordo com o grade se o estudando está aprovado ou não.
+// const students = [
+//     { name: 'Maria', grade: 70, approved: '' },
+//     { name: 'José', grade: 56, approved: '' },
+//     { name: 'Roberto', grade: 90, approved: '' },
+//     { name: 'Ana', grade: 81, approved: '' },
+//   ];
+
+//   function verifyGrades() {
+//     for (let i = 0; i < students.length; i += 1) {
+//       const student = students[i];
+//       if (student.grade >= 60) {
+//         student.approved = 'Aprovado';
+//       } else {
+//         student.approved = 'Recuperação';
+//       }
+//     }
+//   }
+
+//   verifyGrades();
+
+//   console.log(students);
+
+// Agora o mesmo exercício usando o forEach:
+
+// const students = [
+//     { name: 'Maria', grade: 70, approved: '' },
+//     { name: 'José', grade: 56, approved: '' },
+//     { name: 'Roberto', grade: 90, approved: '' },
+//     { name: 'Ana', grade: 81, approved: '' },
+// ];
+
+// function verifyGrades() {
+//     students.forEach((student, index) => {
+//         if (student.grade >= 60) {
+//             students[index].approved = 'Aprovado';
+//         } else {
+//             students[index].approved = 'Recuperação';
+//         }
+//     });
+// }
+
+// verifyGrades();
+
+// console.log(students);
+
+//* outro exercício: encontrar o primeiro número que seja divisível por 5 em uma lista de números.
+
+// const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// let firstMultipleOf5;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 5 === 0) {
+//         firstMultipleOf5 = numbers[index];
+//         break;
+//     }
+// }
+
+// console.log(firstMultipleOf5);
+// 50
+
+//* Resolvendo com o arrayFind: 
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+console.log(firstMultipleOf5);
+// 50
+
+//* Estrutura de HOF em arrays: 
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element) => {
+  console.log('Cada elemento do array:', element);
+});
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+// Outro exemplo usando forEach
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+});
+
+// => ---------------
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+
+//* Usando o forEach pra fazer uma tabuada do 2: 
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+// Outro exercicio
+
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  };
+  
+  // Adicione seu código aqui
+
+emailListInData.forEach(showEmailList);
+
+//* utilizando o FIND: 
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30
