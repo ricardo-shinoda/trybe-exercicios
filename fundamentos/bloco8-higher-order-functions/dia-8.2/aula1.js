25e28e627cd0b39035b460baa3cc2984184b23bc
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-04-11 22:07:30 -0300

    aula 8.2

--- fundamentos/bloco8-higher-order-functions/dia-8.2/aula1.js
+++ fundamentos/bloco8-higher-order-functions/dia-8.2/aula1.js
@@ -186,3 +186,103 @@ const musicas = [
   }
   
   console.log(findMusic('31031685'));
+
+  // 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+  const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+  const hasName = (arr, name) => {
+    //Adicione seu código aqui
+    return arr.some((currentName) => currentName === name);
+  }
+  
+  console.log(hasName(names, 'Ana'));
+
+
+// encontrar uma pessoa pelo ano de nascimento dela: 
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+function authorBornIn1947() {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+}
+
+console.log(authorBornIn1947());
+
+
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+  arrayOfPeople.filter((people, ) => (people.age < 18))
+);
+
+console.log(verifyAgeDrive(objPeople));
+// [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
