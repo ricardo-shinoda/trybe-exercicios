3d6be2fc8042ee78dc68a63f512fb3c435c7ba89
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-04-14 13:18:21 -0300

    exercicio do dia 8.1

--- fundamentos/bloco8-higher-order-functions/dia-8.1/exercicioDoDia.js
+++ fundamentos/bloco8-higher-order-functions/dia-8.1/exercicioDoDia.js
@@ -1,3 +1,5 @@
+//* 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
 const employeeGenerator = (fullName) => {
     const email = fullName.toLowerCase().split(' ').join('_');
     return { fullName, email: `${email}@trybe.com` };
@@ -14,7 +16,10 @@ const employeeGenerator = (fullName) => {
   
   console.log(newEmployees(employeeGenerator));
 
-  // exercício 2:
+
+
+
+  //* exercício 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").:
 
   const numberChecker = (myNumber, number) => myNumber === number;
 
@@ -27,7 +32,12 @@ const employeeGenerator = (fullName) => {
   
   console.log(lotteryResult(3, numberChecker));
 
-  // Exercício 3: 
+
+
+
+
+
+  //* Exercício 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas. Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.: 
 
   const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
@@ -50,3 +60,4 @@ const countPoints = (rightAnswers, studentAnswers, action) => {
 };
 
 console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
