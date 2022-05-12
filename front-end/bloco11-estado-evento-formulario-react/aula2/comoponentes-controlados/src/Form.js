2859259c18465d6cb43c8b9e27eb9b9a23441910
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-05-12 10:24:21 -0300

    controled components

--- front-end/bloco11-estado-evento-formulario-react/aula2/comoponentes-controlados/src/Form.js
+++ front-end/bloco11-estado-evento-formulario-react/aula2/comoponentes-controlados/src/Form.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
   render() {
+    const { email } = this.state;
     return (
       <div>
         <h1> Quiz form </h1>
         <form>
           <label>Name: <input id="name" name="name" text="text" /></label>
-          <label>Email: <input id="email" name="email" text="email" /></label>
+          <label>Email: <input id="email" name="email" type="email" onChange={this.handleChange} value={email} /></label>
           <label>Age: <select id="age" name="age" defaultValue="">
             <option value="">Selecione</option>
             <option value="adult">Maior que 18</option>
