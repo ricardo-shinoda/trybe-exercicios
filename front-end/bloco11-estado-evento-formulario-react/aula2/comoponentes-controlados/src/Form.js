e81bd6438d0c02e313f7d4df9ef23645c0725d28
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-05-12 10:59:17 -0300

    full controled components

--- front-end/bloco11-estado-evento-formulario-react/aula2/comoponentes-controlados/src/Form.js
+++ front-end/bloco11-estado-evento-formulario-react/aula2/comoponentes-controlados/src/Form.js
@@ -4,33 +4,79 @@ class Form extends React.Component {
   constructor() {
     super();
     this.state = {
+      name: '',
       email: '',
+      age: '',
+      checkbox: false,
+      textField: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
-  
-  handleChange(event) {
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
     this.setState({
-      email: event.target.value,
+      [name]: value,
     });
   }
   render() {
-    const { email } = this.state;
+    const { name, email, age, textField, checkbox } = this.state;
     return (
       <div>
         <h1> Quiz form </h1>
         <form>
-          <label>Name: <input id="name" name="name" text="text" /></label>
-          <label>Email: <input id="email" name="email" type="email" onChange={this.handleChange} value={email} /></label>
-          <label>Age: <select id="age" name="age" defaultValue="">
-            <option value="">Selecione</option>
-            <option value="adult">Maior que 18</option>
-            <option value="underage">Menor que 18</option>
-          </select>
+          <label>Name:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+              value={name}
+            />
+          </label>
+          
+          <label>Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              value={email}
+            />
           </label>
 
-          <label htmlFor="textField" >Text Field:<textarea id="textField" name="textField" />
+          <label>Age:
+            <select
+              id="age"
+              name="age"
+              defaultValue=""
+              onChange={this.handleChange}
+              value={age}>
+              <option value="">Selecione</option>
+              <option value="adult">Maior que 18</option>
+              <option value="underage">Menor que 18</option>
+            </select>
+          </label>
 
+          <label>Vai comparecer?
+            <input
+              id="checkbox"
+              name="vaiComparecer"
+              type="checkbox"
+              onChange={this.handleChange}
+              value={checkbox}
+            />
+          </label>
+
+          <label htmlFor="textField">Text Field:
+            <textarea
+              id="textField"
+              name="textField"
+              onChange={this.handleChange}
+              value={textField}
+            />
           </label>
         </form>
       </div >
