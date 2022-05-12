b7f5a974e8b35ced8a07db0197437b05f19a9a3d
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-05-11 22:01:37 -0300

    aula 11.2 creating form

---
+++ front-end/aula2/comoponentes-controlados/src/Form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Quiz form </h1>
+        <form>
+          <label>Name: <input id="name" name="name" text="text" /></label>
+          <label>Email: <input id="email" name="email" text="email" /></label>
+          <label>Age: <select id="age" name="age" defaultValue="">
+            <option value="">Selecione</option>
+            <option value="adult">Maior que 18</option>
+            <option value="underage">Menor que 18</option>
+          </select>
+          </label>
+
+          <label htmlFor="textField" >Text Field:<textarea id="textField" name="textField" />
+
+          </label>
+        </form>
+      </div >
+    );
+  }
+}
+
+export default Form;
