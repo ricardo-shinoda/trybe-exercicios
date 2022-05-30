5cea167f128230ab6c68ce135ae36b6dec43eb02
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-05-30 10:22:54 -0300

    studying props

--- front-end/bloco10-react/aula1/ex1/src/Data.js
+++ front-end/bloco10-react/aula1/ex1/src/Data.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import App from './App';
 
 //! Using class
-class Data extends React.Component {
-    render(props) {
-        const name = props.name;
-        return (
-            <div>
-                <p>Hey there! May name is {name}</p>
-            </div>
-        )
-    }
-}
-
-//! using function
-// function Data(props) {
-//     const name = props.name;
-//     return (
-//         <div>
-//             <p> Oi, meu nome é {name} </p>
-//         </div>
-//     )
+// class Data extends React.Component {
+//     render(props) {
+//         // console.log("render.props")
+//         const name = props.name;
+//         return (
+//             <div>
+//                 <p>Hey there! May name is {name}</p>
+//             </div>
+//         )
+//     }
 // }
 
-
+//! using function
+function Data(props) {
+    console.log("test")
+    const name = props.name;
+    return (
+        <div>
+            <p> Oi, meu nome é {name} </p>
+        </div>
+    )
+}
 
 export default Data;
