62346e20876b6b215f0b973e9b517b282c4db5a3
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-08-19 22:13:41 -0300

    exercicio: 5

--- back-end/node/exercicio-do-dia-22.1/bmi.js
+++ back-end/node/exercicio-do-dia-22.1/bmi.js
@@ -1,24 +1,45 @@
 const readline = require('readline-sync')
 
-const peso = readline.questionFloat(`What's your weight? `);
-const alt = readline.questionInt(`What's your height? `);
-
-// const weightKg = 79;
-// const heightCm = 174;
+const peso = readline.questionFloat(`What's your weight in kg? `);
+const alt = readline.questionInt(`What's your height in centimeter? `);
 
 function handleBmi(peso, alt) {
     console.log(`weight: ${peso}, height: ${alt}`);
 
-    const heightMeter = alt / 100;
+    // const heightMeter = alt / 100;
     const heightSquare = alt * 2;
-    
+
     const bmi = peso / heightSquare;
 
     return bmi;
-} 
+}
 
-function main () {
+
+function main() {
     const bmi = handleBmi(peso, alt);
     console.log(`BMI: ${bmi.toFixed(2)}`)
+
+    if (bmi < 0.185) {
+        console.log('Abaixo do peso(magreza)')
+        return;
+    }
+    if (bmi >= 0.185 && bmi <= 0.249) {
+        console.log('Peso normal')
+        return;
+    }
+    if (bmi >= 0.25 && bmi <= 0.299) {
+        console.log('Acima do peso(sobrepeso)')
+        return;
+    }
+    if (bmi >= 0.30 && bmi <= 0.349) {
+        console.log('Obesidade grau I')
+        return;
+    }
+    if (bmi >= 0.35 && bmi <= 0.399) {
+        console.log('Obesidade grau II')
+        return;
+    }
+        console.log('Obesidade graus III e IV')
 }
+
 main();
